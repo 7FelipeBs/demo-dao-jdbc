951c5b475c325499c4664b8e930b10c22b5a419a
7felipebs <programming1129@gmail.com>
2020-10-16 07:53:33 -0300

    Alteration DaoFactory, create DepartmentDaoJDBC impl

---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
+	}
+}
